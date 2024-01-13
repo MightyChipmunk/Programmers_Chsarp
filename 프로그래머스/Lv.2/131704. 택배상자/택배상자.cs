43887045177e4cd53fc2ee52540df5d1e3c2c0e6
Jihoon <35093963+MightyChipmunk@users.noreply.github.com>
2024-01-13 11:45:21 +0900

    [level Lv.2] Title: 택배상자, Time: 14.35 ms, Memory: 50 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.2/131704. 택배상자/택배상자.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+
+public class Solution {
+    public int solution(int[] order) {
+        int answer = 0;
+        Stack<int> stack = new Stack<int>();
+        int max = 0;
+        
+        for (int i = 0; i < order.Length; i++) {
+            if (i == 0) {
+                for (int j = 1; j < order[i]; j++) {
+                    stack.Push(j);
+                }
+                answer++;
+                max = order[i];
+            }
+            else if (stack.Count > 0 && stack.Peek() == order[i]) {
+                stack.Pop();
+                answer++;
+            }
+            else if (order[i] > order[i - 1]) {
+                for (int j = max + 1; j < order[i]; j++) {
+                    stack.Push(j);
+                }
+                answer++;
+                max = order[i];
+            }
+            else
+                break;
+        }
+        
+        return answer;
+    }
+}
